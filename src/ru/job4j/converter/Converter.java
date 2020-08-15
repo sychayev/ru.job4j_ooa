e87c4c99b46c42ad1ed480dd63a881d2d4aa922d
petrs <sychayev.petr>
2020-08-15 21:10:10 +0300

    5. Конвертер валюты [#343173]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -10,7 +10,6 @@ public class Converter {
         int rs2 = value/72;
         return rs2;
     }
-
     public static void main(String[] args) {
         System.out.println(Converter.rubleToEuro(34568));
         System.out.println(Converter.rubleToDollar(83476));
