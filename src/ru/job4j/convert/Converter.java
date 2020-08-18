bf1e16914270562d4257dbccf336dcae6288bc49
petrs <sychayev.petr>
2020-08-18 21:54:51 +0300

    0. Что такое тестирование [#343212]

--- src/ru/job4j/convert/Converter.java
+++ src/ru/job4j/convert/Converter.java
@@ -3,31 +3,25 @@ package ru.job4j.convert;
 public class Converter {
 
     public static int rubleToEuro(int value) {
-        int rs1 = value / 80;
+        int rs1 = value / 70;
         return rs1;
     }
 
-    public static int rubleToDollar(int value) {
-        int rs2 = value / 72;
-        return rs2;
+//    public static int rubleToDollar(int value) {
+//        int rs2 = value / 60;
+//        return rs2;
+//
+//    }
 
+    public static int rubleToDollar(int value) {
+        return -1;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(Converter.rubleToEuro(160));
-        int in1 = 160;
-        int expected1 = 2;
-        int out1 = rubleToDollar(in1);
-        boolean passed = expected1 == out1;
-        System.out.println("160 rubles are 2 euro.Test result : " + passed);
+        System.out.println(Converter.rubleToEuro(140));
 
-        System.out.println(Converter.rubleToDollar(220));
-        int in2 = 220;
-        int expected2 = 3;
-        int out2 = rubleToDollar(in2);
-        boolean passed2 = expected2 == out2;
-        System.out.println("220 rubles are 3 dollars.Test result : " + passed);
+        System.out.println(Converter.rubleToDollar(180));
 
     }
 
---
+++ test/ru/job4j/convert/ConverterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.convert;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert160RblThen2ToEuro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        int in = 180;
+        int expected = 3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
