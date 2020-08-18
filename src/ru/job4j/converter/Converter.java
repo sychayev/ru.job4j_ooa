00d66175852dcdee7f2335b56310d3eef6fc2301
petrs <sychayev.petr>
2020-08-18 19:34:22 +0300

    9. Площадь треугольника. [#343180]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -13,8 +13,22 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        System.out.println(Converter.rubleToEuro(34568));
-        System.out.println(Converter.rubleToDollar(83476));
+        System.out.println(Converter.rubleToEuro(160));
+        System.out.println(Converter.rubleToDollar(150));
+
+        int in1 = 160;
+        int expected1 = 2;
+        int out1 = rubleToDollar(in1);
+        boolean passed = expected1 == out1;
+        System.out.println("34568 rubles are2.Test result : " + passed);
+
+        int in2 = 1175;
+        int expected2 = 2;
+        int out2 = rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("34568 rubles are2.Test result : " + passed);
+
     }
+
 
 }
