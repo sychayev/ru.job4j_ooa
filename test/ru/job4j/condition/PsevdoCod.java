b47ac95d29c349d3ccc37c82dd4355a1646a4ddc
petrs <sychayev.petr>
2020-08-21 10:57:35 +0300

    4.1.4. Псевдокод [#343129]

--- test/ru/job4j/condition/PsevdoCod.java
+++ test/ru/job4j/condition/PsevdoCod.java
@@ -3,6 +3,12 @@ package ru.job4j.condition;
 public class PsevdoCod {
     public static void main(String[] args) {
 
+        //    Обьявили A>B,B=C
+//    if(A > B) Вывести на экран А;
+//    else      Вывести на экран B;
+//
+//    else if(A > C)  Вывести на экран А;
+//     else           Вывести на экран C;
         int A = 3, B = 1, C = 1;
         if (A > B) {
             System.out.println("A is bigger than B!");
@@ -14,16 +20,6 @@ public class PsevdoCod {
         } else {
             System.out.println("C is bigger than A!");
         }
-
-
-//    Обьявили A>B,B=C
-//    if(A > B) Вывести на экран А;
-//    else      Вывести на экран B;
-//
-//    else if(A > C)  Вывести на экран А;
-//     else           Вывести на экран C;
-
-
     }
 
 }
