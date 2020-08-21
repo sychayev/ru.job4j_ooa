874130c3c27750f84375f6939d4c9557f8a25802
petrs <sychayev.petr>
2020-08-21 09:20:45 +0300

    4.1.2. Глупый бот [#343120]

---
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        assertThat(
+                DummyBot.answer("Привет,Бот."),
+                is("Привет,умник")
+        );
+
+    }
+
+    @Test
+    public void whenByeBot() {
+        assertThat(
+                DummyBot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenunknownBot() {
+        assertThat(
+                DummyBot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+}
