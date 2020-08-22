0d28b11212bff21c2b9e4e6a24081ae52cacfdab
petrs <sychayev.petr>
2020-08-22 12:56:54 +0300

    4.3.4. if c return [#343132]

---
+++ test/ru/job4j/condition/PointEq.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class PointEq {
+
+    public static boolean eq(int x1,int y1,int x2,int y2 ){
+        boolean eqX = x1 == x2;
+        boolean eqY = y1 == y2;
+//        if(eqX && eqY){
+//            return true;
+//        }else {
+//            return false;
+//        }
+        boolean result = eqX && eqY;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(eq(1,1,2,2));
+        System.out.println(eq(10,10,10,10));
+    }
+}
