50b535684b8b113ac1b078df9a3f59eb68addae6
petrs <sychayev.petr>
2020-08-23 08:52:20 +0300

    4.2. Тернарное сравнение [#343136]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -8,14 +8,21 @@ import static org.junit.Assert.*;
 public class MaxTest {
 
     @Test
-    public void max() {
+    public void whenLeftLessRight() {
 
         int res = Max.max(2, 9);
         assertThat(res, is(9));
-        int res1 = Max.max(9, 9);
-        assertThat(res, is(9));
-        int res2 = Max.max(9, 2);
-        assertThat(res, is(9));
+    }
 
+    @Test
+    public void whenLeftEqualsRight() {
+        int res = Max.max(9, 9);
+        assertThat(res, is(9));
+    }
+
+    @Test
+    public void whenLeftBiggerThanRight() {
+        int res = Max.max(9, 2);
+        assertThat(res, is(9));
     }
 }
---
+++ test/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+
+//    public static int max(int first, int second, int third) {
+//        String s = "All values are little than third";
+//        int res1 = (first > second && second < third) && (first < third) ? third : Integer.parseInt(s);
+//        int res2 = (second > first && third <third ) && (first < third) ? third : Integer.parseInt(s);
+//        int res3 = (first > second && second < third) && (first < third) ? third : Integer.parseInt(s);
+//        return;
+//    }
+
+}
