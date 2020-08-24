656c48dd5202ce8da8466d713611dcf780feccdc
petrs <sychayev.petr>
2020-08-24 19:32:46 +0300

    5.0.2. Вечный цикл for.

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -1,5 +1,17 @@
 package ru.job4j.condition;
 
 public class MultiMax {
+    public static int max(int first, int second, int third) {
+        int result = first;
+        int firstMax = ((first > second && second < third)||(first >third))?first:third;
+//        int secondMax = ((second > first && first < third)&&(second > third))?second:firstMax;
+//        int thirdMax= (third > second && first < second)&&(third > first)?third:secondMax ;
+        return first;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(MultiMax.max(10, 8, 94));
+        System.out.println(MultiMax.max(45, 23, 43));
+        System.out.println(MultiMax.max(76, 31, 23));
+    }
 }
