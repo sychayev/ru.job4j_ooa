25bc03039ee7e2fc9ceb8ca33187e664c6400775
petrs <sychayev.petr>
2020-08-25 15:23:22 +0300

    4.4. Вложенный оператор if else [#343137]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,16 +2,16 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int first, int second, int third) {
-        int result = first;
-        int firstMax = ((first > second && second < third)||(first >third))?first:third;
-//        int secondMax = ((second > first && first < third)&&(second > third))?second:firstMax;
-//        int thirdMax= (third > second && first < second)&&(third > first)?third:secondMax ;
-        return first;
+        int result= first ;
+        int firstMax = ((first > second || second <third)&&(first > third))?first:third;
+        int secondMax = ((second > first || first < third)&&(second > third))?second:firstMax;
+        int thirdMax= ((third > firstMax || firstMax < secondMax)&&(third > secondMax ))?third:secondMax ;
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(MultiMax.max(10, 8, 94));
-        System.out.println(MultiMax.max(45, 23, 43));
-        System.out.println(MultiMax.max(76, 31, 23));
+        System.out.println(MultiMax.max(100, 8, 94));
+        System.out.println(MultiMax.max(45, 63, 43));
+        System.out.println(MultiMax.max(76, 31, 93));
     }
 }
---
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MultiMaxTest {
+
+    @Test
+    public void max() {
+    }
+
+}
