76ef300a10f0b1596fe919960e1b3eac85527934
petrs <sychayev.petr>
2020-08-26 18:58:53 +0300

    5.8. Именование классов и переменных

--- src/ru/job4j/loop/User.java
+++ src/ru/job4j/loop/User.java
@@ -4,6 +4,7 @@ public class User {
     public static void convert() {
         String name = "Petr Sychaev";
         System.out.println(name);
+
     }
 
     public static void main(String[] args) {
