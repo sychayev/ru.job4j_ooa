06d7e32886f081118960d73c0b7391118f1528ad
petrs <sychayev.petr>
2020-08-27 09:31:38 +0300

    5.5. Простое число

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -10,7 +10,6 @@ public class CheckPrimeNumber {
                 prime = false;
                 break;
             } else {
-                prime = true;
                 break;
             }
         }
