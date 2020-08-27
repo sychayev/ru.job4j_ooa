4ddad08af4f9589a13c339f6bec50d08779607d5
petrs <sychayev.petr>
2020-08-27 16:55:13 +0300

    6.0.2. Заполнение массива.

--- src/ru/job4j/array/ArrayDefinision.java
+++ src/ru/job4j/array/ArrayDefinision.java
@@ -3,12 +3,22 @@ package ru.job4j.array;
 public class ArrayDefinision {
 
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Ages - " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Surnames - " + surnames.length);
-        float[] prices = new float[40];
-        System.out.println("Prices - " + prices.length);
+//        short[] ages = new short[10];
+//        System.out.println("Ages - " + ages.length);
+//        String[] surnames = new String[100500];
+//        System.out.println("Surnames - " + surnames.length);
+//        float[] prices = new float[40];
+//        System.out.println("Prices - " + prices.length);
+
+
+        String[] s = new String[4];
+        s[0] = "Hi";
+        s[1] = " my";
+        s[2] = " name";
+        s[3] = " Petr.";
+        System.out.println(s[3]);
+
+
     }
 
 }
