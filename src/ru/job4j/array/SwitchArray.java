49249caebf3f153ae2d1c7704b2ba7073f85cb1d
petrs <sychayev.petr>
2020-08-29 16:50:16 +0300

    6.1. Классический поиск перебором.

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -8,14 +8,14 @@ public class SwitchArray {
     public static int[] swapBorder(int[] array) {
 
         int temp = array[0];
-        array[0] = array[array.length];
-        array[array.length] = temp;
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
         return array;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-//        int[] rsl = SwitchArray.swapBorder(nums);
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        int[] rsl = SwitchArray.swapBorder(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.println(nums[i]);
         }
