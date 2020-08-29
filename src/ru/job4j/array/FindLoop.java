5d8aeedc010b71b6c4f7043e784e107eaf95e564
petrs <sychayev.petr>
2020-08-29 14:04:45 +0300

    6.2.1. Выход за границу массива.

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,11 +1,13 @@
 package ru.job4j.array;
 
 public class FindLoop {
+
     public static int indexOf(int[] data, int el) {
         int rst = -1;
         for (int i = 1; i < data.length; i++) {
             if (data[i] == el) {
-                rst = i;
+                rst = data[i];
+//                System.out.println(rst);
                 break;
             }
         }
@@ -13,6 +15,10 @@ public class FindLoop {
     }
 
     public static void main(String[] args) {
-//        FindLoop.indexOf(8, 3);
+        int[] num = new int[]{1, 2, 3, 4, 5};
+//        int[] rsl  = FindLoop.indexOf(num);
+        System.out.println(FindLoop.indexOf(num, 3));
     }
+
+
 }
