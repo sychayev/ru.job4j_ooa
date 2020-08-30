c78c91e1665f769dd0e128d14378502040695923
petrs <sychayev.petr>
2020-08-30 15:07:43 +0300

    5.7. Ипотека

--- src/ru/job4j/loop/Morgage.java
+++ src/ru/job4j/loop/Morgage.java
@@ -5,7 +5,7 @@ public class Morgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount += percent;
+            amount += (amount *= percent / 100);
             amount -= salary;
             year++;
         }
