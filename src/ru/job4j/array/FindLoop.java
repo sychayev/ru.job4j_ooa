9571b53e929c297c85aeecf4f2909fb2e15479cd
petrs <sychayev.petr>
2020-09-05 14:04:34 +0300

    6.5. Поиск индекса в диапазоне.

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -2,24 +2,39 @@ package ru.job4j.array;
 
 public class FindLoop {
 
-    public static int indexOf(int[] data, int el) {
+    public static int indexOf1(int[] data, int el) {
         int rst = -1;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
                 rst = i;
                 break;
-
             }
         }
+        return rst;
+    }
 
+    public static int indexOf2(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = start; i < data.length; i++) {
+//            if (data[i] == data[el]) {
+//                rst = i;
+//            }
+            if (data[i] == data[start]) {
+                rst = i;
+            }
+            if (data[i] == data[finish]) {
+                rst = i;
+            }
+            rst = i - 1;
+        }
         return rst;
     }
 
     public static void main(String[] args) {
         int[] num = new int[]{5, 4, 3, 2};
-        System.out.println(FindLoop.indexOf(num, 5));
-        int[] num1 = new int[]{89, 2, 34, 54, 4};
-        System.out.println(FindLoop.indexOf(num1, 2));
+        System.out.println(FindLoop.indexOf1(num, 5));
+        int[] num1 = new int[]{5, 2, 10, 2, 4};
+        System.out.println(FindLoop.indexOf2(num1, 2, 2, 4));
 
     }
 
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -8,20 +8,22 @@ import static org.junit.Assert.*;
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas5Then0() {
-        int[] input = {5, 4, 3, 2};
+    public void whenArrayHasLength5Then0() {
+        int[] input = {5, 10, 3};
         int value = 5;
-        int result = FindLoop.indexOf(input, value);
-        int expect = -1;
+        int result = FindLoop.indexOf1(input, value);
+        int expect = 0;
         assertThat(result, is(expect));
     }
 
     @Test
     public void thenArrayHas2Then2() {
-        int[] input = {89, 2, 34, 54, 4};
+        int[] input = new int[]{5, 2, 10, 2, 4};
         int value = 2;
-        int result = FindLoop.indexOf(input, value);
-        int expect = 34;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf2(input, value, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
 }
