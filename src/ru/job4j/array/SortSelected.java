b76cdde2b67faa464d4e487ab1f9cbbee675b4df
petrs <sychayev.petr>
2020-09-08 21:01:22 +0300

    6.5.3. Сортировка выборкой.

---
+++ src/ru/job4j/array/SortSelected.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+
+        for (int i = 0; i < data.length - 1; i++) {
+            int min = MinDiapazon.findMin(data, 0, data.length);
+            int index = FindLoop.indexOf2(data, min, 0, data.length);
+//            if (data[i] < data[i + 1]) {
+//                int temp = data[i];
+//                data[i] = data[i + 1];
+//                data[i + 1] = temp;
+//            }
+//            if (min < index + 1) {
+//                int temp = data[min];
+//                data[min] = data[min + 1];
+//                data[min + 1] = temp;
+//            }
+            if (min < index+ 1) {
+                int temp = data[index];
+                data[index] = data[min + 1];
+                data[min + 1] = temp;
+
+            }
+        }
+        return data;
+    }
+
+    public static void main(String[] args) {
+        int mas1[] = new int[]{3, 1, 2};
+        System.out.println(SortSelected.sort(mas1));
+        int mas2[] = new int[]{8, 7, 9, 6, 1};
+        System.out.println(SortSelected.sort(mas2));
+        int mas3[] = new int[]{23, 10, 45, 2};
+        System.out.println(SortSelected.sort(mas3));
+    }
+}
---
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SortSelectedTest {
+
+    @Test
+    public void whenSort1() {
+        int[] input = new int[]{3,  1, 2};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[]{1, 2, 3};
+        assertThat(result, is(expect)
+        );
+    }
+
+    @Test
+    public void whenSort2() {
+        int[] input = new int[]{8, 7, 9, 6, 1};
+        int[] result = SortSelected.sort(input);
+        int expect[] = new int[]{1, 6, 7, 8, 9};
+        assertThat(result, is(expect));
+
+    }
+
+    @Test
+    public void whenSort3() {
+        int[] input = new int[]{23, 10, 45, 2};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[]{2, 10, 23, 45};
+        assertThat(result, is(expect));
+
+    }
+}
