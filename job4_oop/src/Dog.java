acc829eee2422af77fbe4a895aa3785f9af181f9
petrs <sychayev.petr>
2020-09-10 18:48:24 +0300

    1.1. Создание объекта

--- job4_oop/src/Dog.java
+++ job4_oop/src/Dog.java
@@ -1,7 +1,7 @@
 public class Dog {
     public static void main(String[] args) {
-        Dog dig = new Dog();
-        Dog dip = new Dog();
-        Dog dik = new Dog();
+        Dog polkan = new Dog();
+        Dog  sharik = new Dog();
+        Dog zhychka = new Dog();
     }
 }
