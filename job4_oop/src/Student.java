f0b56740b719dba37b7d86ac20c42c97e1a97af6
petrs <sychayev.petr>
2020-09-11 17:36:03 +0300

    1.1. Создание объекта

--- job4_oop/src/Student.java
+++ job4_oop/src/Student.java
@@ -3,7 +3,6 @@ public class Student {
     public void  letuchka(){
         System.out.println("I can fill i can fly");
     }
-
     public static void main(String[] args) {
         Student study = new Student();
         study.letuchka();
