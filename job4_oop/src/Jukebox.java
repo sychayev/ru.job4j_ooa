ce9ac1c2009d38c21e7ff47c90bdfb74edc6621f
petrs <sychayev.petr>
2020-09-12 19:52:09 +0300

    1.1. Создание объекта

---
+++ job4_oop/src/Jukebox.java
@@ -0,0 +1,15 @@
+public class Jukebox {
+    public static void music(int position){
+        if(position == 1){
+            System.out.println("Пусть бегут неуклюже");
+        }else if(position == 2){
+            System.out.println( "Спокойной ночи");
+        }else{
+            System.out.println( "Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox.music(1);
+    }
+}
