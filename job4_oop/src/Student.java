47ec8d8a9eece26b51069b7ff11ae9733a379d48
petrs <sychayev.petr>
2020-09-12 10:08:20 +0300

    1.2. Вызов метода объекта

--- job4_oop/src/Student.java
+++ job4_oop/src/Student.java
@@ -1,13 +1,15 @@
 public class Student {
 
-    public void  letuchka(){
+    public void music() {
         System.out.println("I believe you can fly");
+        System.out.println("Petya  play and sing for us please!");
+        System.out.println("Petya  play and sing for us please!");
+        System.out.println("Petya  play and sing for us please!");
     }
+
     public static void main(String[] args) {
         Student study = new Student();
-        study.letuchka();
-        System.out.println("Petya  play and sing for us please!");
-        System.out.println("Petya  play and sing for us please!");
-        System.out.println("Petya  play and sing for us please!");
+        study.music();
+
     }
 }
