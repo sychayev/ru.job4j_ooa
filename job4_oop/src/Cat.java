df1eca64e6c2e7644ad4e3ca7fe9dce8830998ae
petrs <sychayev.petr>
2020-09-13 06:36:09 +0300

    1.5. Поля объекта

--- job4_oop/src/Cat.java
+++ job4_oop/src/Cat.java
@@ -1,6 +1,30 @@
 public class Cat {
+    private String food;
+    private String name;
+    public void givNick(String nick){
+        this.name = nick;
+        System.out.println(this.name);
+
+    }
+    public  void show(){
+        System.out.println(this.food);
+
+    }
+
+    public void eat(String meat){
+        this.food = meat;
+    }
     public static void main(String[] args) {
-        Cat perry = new Cat();
-        Cat petty = new Cat();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.givNick("Barsik eat fish");
+        gav.eat("Kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.givNick("Persik eat bird");
+        black.eat("Fish");
+        black.show();
+
     }
 }
