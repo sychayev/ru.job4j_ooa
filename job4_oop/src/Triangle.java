b4d5f10495c77faeb191cb42d42d6334656fb898
petrs <sychayev.petr>
2020-09-17 19:21:38 +0300

    4.6.2. Switch - matcher

--- job4_oop/src/Triangle.java
+++ job4_oop/src/Triangle.java
@@ -1,38 +1,42 @@
 
 public class Triangle {
-//    private Point first;
-//    private Point second;
-//    private Point third;
-//
-//    public Triangle(Point ap, Point bp, Point cp) {
-//        this.first = ap;
-//        this.second = bp;
-//        this.third = cp;
-//    }
-//
-//    public double period(double a, double b, double c) {
-//
-//        return -1;
-//    }
-//
-//    public boolean exist(double ab, double ac, double dc) {
-//        return false;
-//    }
-//
-//    public double area() {
-//        double rsl = -1;
-//        double ab = first.distance(second);
-//        double ac = first.distance(third);
-//        double bc = second.distance(third);
-//        double ap = period(ab, ac, bc);
-//        if (this.exist(ab, ac, bc)) {
-//
-//
-//            rsl = -1;
-//        }
-//    }
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        double prd = (a + b + c) / 2;
+        return prd;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        boolean corner1 = ab < ac + bc;
+        boolean corner2 = ac < ab + bc;
+        boolean corner3 = bc < ab + ac;
+        if (corner1 != corner2 != corner3){
+            }
+        return false;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) - (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
 
     public static void main(String[] args) {
-
+//        Triangle trg = new Triangle(0,4,4);
     }
 }
