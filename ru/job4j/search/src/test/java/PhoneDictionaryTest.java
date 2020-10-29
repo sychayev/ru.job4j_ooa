9457f76b09b18330e77f3f91908d5af72bb4f8ea
petrs <sychayev.petr>
2020-10-29 14:25:28 +0300

    2. Очередь с приоритетом на LinkedList

---
+++ ru/job4j/search/src/test/java/PhoneDictionaryTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+
+        @Test
+        public void whenFindByName() {
+            PhoneDictionary phones = new PhoneDictionary();
+            phones.add(
+                    new Person("Petr",
+                            "Sychaev",
+                            "53742", "Hadera")
+            );
+            ArrayList<Person> persons = phones.find("Petr");
+            assertThat(persons.get(0).getSurname(), is("Sychaev"));
+        }
+
+}
